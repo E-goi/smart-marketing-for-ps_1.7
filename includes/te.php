5fa18d22295f3827ea38454e080bc6b10595ce30
MS <msousa@e-goi.com>
2018-09-11 16:38:12 +0100

    FIX identation for Prestashop Validator

--- includes/te.php
+++ includes/te.php
@@ -17,71 +17,71 @@ $te = "<script type='text/javascript'>
 		
 		$sum_price = '';
 
-		if(!empty($products)){
-			foreach($products as $key => $product){
-			
-				$product_id = $product['id_product'];
-		 		$product_name = htmlentities($product['name']);
-		 		$product_cat = '-';
-		 		$product_price = floatval($product['price_wt']);
-		 		$product_quantity = $product['quantity'];
+if(!empty($products)){
+    foreach($products as $key => $product){
 
-		 		$sum_price += floatval(round(($product_price * $product_quantity), 2));
+        $product_id = $product['id_product'];
+        $product_name = htmlentities($product['name']);
+        $product_cat = '-';
+        $product_price = floatval($product['price_wt']);
+        $product_quantity = $product['quantity'];
 
-				$te .= "_egoiaq.push(['addEcommerceItem',
-			    \"$product_id\",
-			    \"$product_name\",
-			    \"$product_cat\",
-			    $product_price,
-			    $product_quantity]);\n";
-			}
+        $sum_price += floatval(round(($product_price * $product_quantity), 2));
 
-		}else if ($cart_zero == 1) {
-			$id = 0;
-			$name = '';
-			$cat = '';
-			$price = 0;
-			$qty = 0;
-			$sum_price = 0;
+        $te .= "_egoiaq.push(['addEcommerceItem',
+        \"$product_id\",
+        \"$product_name\",
+        \"$product_cat\",
+        $product_price,
+        $product_quantity]);\n";
+    }
 
-			$te .= "_egoiaq.push(['addEcommerceItem',
-		    	\"$id\",
-		    	\"$name\",
-		    	\"$cat\",
-		    	$price,
-		    	$qty]);\n";
+}else if ($cart_zero == 1) {
+    $id = 0;
+    $name = '';
+    $cat = '';
+    $price = 0;
+    $qty = 0;
+    $sum_price = 0;
 
-		    $te .= "_egoiaq.push(['trackEcommerceCartUpdate',
-			    	0\n
-			    ]);\n";
-		}
+    $te .= "_egoiaq.push(['addEcommerceItem',
+        \"$id\",
+        \"$name\",
+        \"$cat\",
+        $price,
+        $qty]);\n";
+
+    $te .= "_egoiaq.push(['trackEcommerceCartUpdate',
+            0\n
+        ]);\n";
+}
+
+if(isset($order)){
+    $order_total = floatval(round($sum_price, 2));
+
+    $te .= "_egoiaq.push(['trackEcommerceOrder',
+    \"$order_id\",
+    \"$order_total\",
+    \"$order_subtotal\",
+    $order_tax,
+    $order_shipping,
+    $order_discount]);\n";
+
+}else{
+
+    if($sum_price){
+
+        $te .= "_egoiaq.push(['trackEcommerceCartUpdate',
+        $sum_price\n
+        ]);\n";
+    }
+}
 		
-		if(isset($order)){
-			$order_total = floatval(round($sum_price, 2));
-
-			$te .= "_egoiaq.push(['trackEcommerceOrder',
-		    \"$order_id\",
-		    \"$order_total\",
-		    \"$order_subtotal\",
-		    $order_tax,
-		    $order_shipping,
-		    $order_discount]);\n";
-		    
-		}else{
-
-			if($sum_price){
-
-				$te .= "_egoiaq.push(['trackEcommerceCartUpdate',
-			    $sum_price\n
-			    ]);\n";
-			}
-		}
-		
-		$te .= "_egoiaq.push(['trackPageView']);
-		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-		g.type='text/javascript';
-		g.defer=true;
-		g.async=true;
-		g.src=u+'egoimmerce.js';
-		s.parentNode.insertBefore(g,s);
-	</script>";
+    $te .= "_egoiaq.push(['trackPageView']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript';
+    g.defer=true;
+    g.async=true;
+    g.src=u+'egoimmerce.js';
+    s.parentNode.insertBefore(g,s);
+</script>";
