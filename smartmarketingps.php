15486f91c8971948214e1586a1d0711559c32d85
Tiago Mota <tmota@e-goi.com>
2021-03-02 19:16:49 +0000

    feat: switch ifthenpay multibanco integration

--- smartmarketingps.php
+++ smartmarketingps.php
@@ -1349,12 +1349,126 @@ class SmartMarketingPs extends Module
      */
     private function getIfThenPayData($order)
     {
-        $module = Module::getInstanceByName('multibanco');
-        $details = $module->getMBDetails();
-        $total = $order->getOrdersTotalPaid();
-        $ref = $module->GenerateMbRef($details[0], $details[1], $order->id, $total);
 
-        return $this->buildMbArray($details[0], $ref, $total, $order->id_currency);
+        $total = $order->getOrdersTotalPaid();
+        if(!empty($total) && is_numeric($total)){
+            $total = sprintf('%0.2f', $total);
+        }
+
+        if($order->module == 'ifthenpay'){//check ifthenpay module
+            $details = (array) unserialize(Configuration::get('IFTHENPAY_USER_ACCOUNT'));
+            $ent = $details[0]['Entidade'];
+            $ref = $this->IfThenPayGenerateMbRef($ent, $details[0]['SubEntidade'][0], $order->id, $total);
+        }else{
+            $module = Module::getInstanceByName('multibanco');
+            $details = $module->getMBDetails();
+            $ent = $details[0];
+            $ref = $module->GenerateMbRef($details[0], $details[1], $order->id, $total);
+        }
+        return $this->buildMbArray($ent, $ref, $total, $order->id_currency);
+    }
+
+
+    /**
+     * IFTHENPAY
+     * @param $ent_id
+     * @param $subent_id
+     * @param $order_id
+     * @param $order_value
+     * @return string
+     */
+    private function IfThenPayGenerateMbRef($ent_id, $subent_id, $order_id, $order_value)
+    {
+
+        $order_id ="0000".$order_id;
+
+        $order_value =  $this->format_number($order_value);
+
+        //Apenas sao considerados os 4 caracteres mais a direita do order_id
+        $order_id = substr($order_id, (strlen($order_id) - 4), strlen($order_id));
+
+
+        while ($order_value >= 1000000){
+            $this->IfThenPayGenerateMbRef($order_id++, 999999.99);
+            $order_value -= 999999.99;
+        }
+
+        //cálculo dos check digits
+        $chk_str = sprintf('%05u%03u%04u%08u', $ent_id, $subent_id, $order_id, round($order_value*100));
+
+        $chk_array = array(3, 30, 9, 90, 27, 76, 81, 34, 49, 5, 50, 15, 53, 45, 62, 38, 89, 17, 73, 51);
+
+        $chk_val=0;
+
+        for ($i = 0; $i < 20; $i++)
+        {
+            $chk_int = substr($chk_str, 19-$i, 1);
+            $chk_val += ($chk_int%10)*$chk_array[$i];
+        }
+
+        $chk_val %= 97;
+
+        $chk_digits = sprintf('%02u', 98-$chk_val);
+
+        return $subent_id." ".substr($chk_str, 8, 3)." ".substr($chk_str, 11, 1).$chk_digits;
+
+    }
+
+
+    /**
+     * IFTHENPAY number format
+     * @param $number
+     * @return string
+     */
+    private function format_number($number)
+    {
+        $verifySepDecimal = number_format(99,2);
+
+        $valorTmp = $number;
+
+        $sepDecimal = substr($verifySepDecimal, 2, 1);
+
+        $hasSepDecimal = True;
+
+        $i=(strlen($valorTmp)-1);
+
+        for($i;$i!=0;$i-=1)
+        {
+            if(substr($valorTmp,$i,1)=="." || substr($valorTmp,$i,1)==","){
+                $hasSepDecimal = True;
+                $valorTmp = trim(substr($valorTmp,0,$i))."@".trim(substr($valorTmp,1+$i));
+                break;
+            }
+        }
+
+        if($hasSepDecimal!=True){
+            $valorTmp=number_format($valorTmp,2);
+
+            $i=(strlen($valorTmp)-1);
+
+            for($i;$i!=1;$i--)
+            {
+                if(substr($valorTmp,$i,1)=="." || substr($valorTmp,$i,1)==","){
+                    $hasSepDecimal = True;
+                    $valorTmp = trim(substr($valorTmp,0,$i))."@".trim(substr($valorTmp,1+$i));
+                    break;
+                }
+            }
+        }
+
+        for($i=1;$i!=(strlen($valorTmp)-1);$i++)
+        {
+            if(substr($valorTmp,$i,1)=="." || substr($valorTmp,$i,1)=="," || substr($valorTmp,$i,1)==" "){
+                $valorTmp = trim(substr($valorTmp,0,$i)).trim(substr($valorTmp,1+$i));
+                break;
+            }
+        }
+
+        if (strlen(strstr($valorTmp,'@'))>0){
+            $valorTmp = trim(substr($valorTmp,0,strpos($valorTmp,'@'))).trim($sepDecimal).trim(substr($valorTmp,strpos($valorTmp,'@')+1));
+        }
+
+        return $valorTmp;
     }
 
     /**
