703d0b85c9adc17a466196b61eab2ced849ca64d
Tiago Mota <tmota@e-goi.com>
2021-03-02 19:21:48 +0000

    doc: generate new version

--- smartmarketingps.php
+++ smartmarketingps.php
@@ -102,7 +102,7 @@ class SmartMarketingPs extends Module
 		// Module metadata
 		$this->name = 'smartmarketingps';
 	    $this->tab = 'advertising_marketing';
-	    $this->version = '1.6.1';
+	    $this->version = '1.6.2';
 	    $this->author = 'E-goi';
 	    $this->need_instance = 1;
 	    $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
