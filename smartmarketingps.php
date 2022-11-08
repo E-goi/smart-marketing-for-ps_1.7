53716cde85db43053574080767da1c1ffb47546c
fbobiano <fbobiano@e-goi.com>
2022-11-08 10:14:37 +0000

    fix(BB-21520): bump version

--- smartmarketingps.php
+++ smartmarketingps.php
@@ -116,7 +116,7 @@ class SmartMarketingPs extends Module
 		// Module metadata
 		$this->name = 'smartmarketingps';
 	    $this->tab = 'advertising_marketing';
-	    $this->version = '2.0.3';
+	    $this->version = '2.0.4';
 	    $this->author = 'E-goi';
 	    $this->need_instance = 1;
 	    $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
