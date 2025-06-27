a8eacfd76aad9604c8f370137024587dfb0ce1b0
Luis Araujo <laraujo@e-goi.com>
2025-06-27 11:48:42 +0100

    improv/BB-27190: update version

--- smartmarketingps.php
+++ smartmarketingps.php
@@ -178,7 +178,7 @@ class SmartMarketingPs extends Module
         // Module metadata
         $this->name = 'smartmarketingps';
         $this->tab = 'advertising_marketing';
-        $this->version = '3.1.0';
+        $this->version = '3.1.1';
         $this->author = 'E-goi';
         $this->need_instance = 1;
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
